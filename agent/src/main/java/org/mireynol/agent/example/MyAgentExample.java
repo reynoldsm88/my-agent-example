73aac7b37cf841fabd6f645db47c724a51b1d241
Michael Reynolds <reynoldsm88@gmail.com>
2018-02-08 14:06:35 -0500

    basic project setup

---
+++ agent/src/main/java/org/mireynol/agent/example/MyAgentExample.java
@@ -0,0 +1,15 @@
+package org.mireynol.agent.example;
+
+import java.lang.instrument.Instrumentation;
+
+public class MyAgentExample {
+
+    public static void premain( String agentArgs, Instrumentation inst ) {
+
+    }
+
+    public static void agentmain( String agentArgs, Instrumentation inst ) {
+
+    }
+
+}
